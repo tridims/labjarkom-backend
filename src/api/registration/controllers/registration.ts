18f47a2f3a5a3eeb02776839fa6681b826bc6fcb
Dimas Tri Mustakim <test31122001@gmail.com>
2023-08-08 10:58:44 +0700

    done registration

--- src/api/registration/controllers/registration.ts
+++ src/api/registration/controllers/registration.ts
@@ -8,20 +8,23 @@ export default factories.createCoreController(
   "api::registration.registration",
   () => ({
     async create(ctx) {
-      const { data } = ctx.request.body();
-      const files = ctx.request.files();
+      const { data } = ctx.request.body;
+      const files = ctx.request.files;
 
       const parsedData = JSON.parse(data);
 
+      console.log("parsedData", parsedData);
+      console.log(files);
+
+      // Cara 1
+      console.log("CREATING REGISTRATION");
       const entry = await strapi.entityService.create(
         "api::registration.registration",
         {
           data: {
             ...parsedData,
           },
-          files: {
-            file: files["file.cv"],
-          },
+          files,
         }
       );
 
